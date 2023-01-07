3c9fa7c3d18baf7709cbc4a6026eaff4c58eaf54
Anna Gosteva <gostevaay@yandex.ru>
2023-01-07 23:30:11 +0300

    add tests

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -27,6 +27,15 @@ public class ManagerTest {
         String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void shouldReturnEmptyArray() {
+
+        Manager manager = new Manager();
+        manager.addFilm(null);
+        String[] expected = {null};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 
 
     @Test
@@ -51,5 +60,45 @@ public class ManagerTest {
 
     }
 
+
+    @Test
+    public void shouldFindLessThanLimit(){
+        Manager manager = new Manager();
+
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+        manager.addFilm("Film 4");
+        manager.addFilm("Film 8");
+        manager.addFilm("Film 9");
+        manager.addFilm("Film 10");
+
+
+        String[] expected = {"Film 10", "Film 9", "Film 8",  "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldNoFindMoreThanLimit(){
+        Manager manager = new Manager();
+
+        manager.addFilm("Film 0");
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+        manager.addFilm("Film 4");
+        manager.addFilm("Film 5");
+        manager.addFilm("Film 6");
+        manager.addFilm("Film 7");
+        manager.addFilm("Film 8");
+        manager.addFilm("Film 9");
+        manager.addFilm("Film 10");
+
+
+
+        String[] expected = {"Film 10", "Film 9", "Film 8","Film 7", "Film 6", "Film 5",  "Film 4", "Film 3", "Film 2", "Film 1","Film 0"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 
 }
